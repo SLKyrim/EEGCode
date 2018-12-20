ca21f686610bece20cebccac800ca879fdd7d38d
Single Long <623962644@qq.com>
2018-12-20 23:10:11 +0800

    RCAR2019
    CSP第一个特征分解处特征值不需要排序，对后续结果不影响

--- Electrode_Selector/eeg_CSP.py
+++ Electrode_Selector/eeg_CSP.py
@@ -11,11 +11,11 @@ Created on Tue Dec 12 11:07:25 2017
 % 通过CSP求区别有无意图的投影矩阵
 % 并通过CSP投影矩阵提取EEG窗方差特征
 """
-
+# In[1]
 import scipy.io as sio
 import numpy as np
 import scipy.linalg as la # 线性代数库
-
+# In[2]
 id_subject = 1 # 【受试者的编号】
 num_pair = 4 # 【从CSP投影矩阵里取得特征对数】
 
@@ -27,9 +27,8 @@ else:
     input_eegwin_dict = sio.loadmat('E:\\EEGExoskeleton\\Data\\Subject_'+\
                                     str(id_subject)+'_Data\\Subject_'+\
                                     str(id_subject)+'_WinEEG.mat')
-
 input_eegwin = input_eegwin_dict['WinEEG']
-
+# In[3]
 eegwin_0 = [] # 存放标记为-1的EEG窗
 eegwin_1 = [] # 存放标记为1的EEG窗
 for i in range(len(input_eegwin)):
@@ -37,15 +36,14 @@ for i in range(len(input_eegwin)):
         # 若EEG窗标记为0
         eegwin_0.append(input_eegwin[i][0])
     elif int(input_eegwin[i][1]) == 1:
-        eegwin_1.append(input_eegwin[i][0])
-        
+        eegwin_1.append(input_eegwin[i][0])  
 task = (eegwin_0, eegwin_1)
-
+# In[4]
 # 获取EEG窗的标准化空间协方差矩阵
 def covarianceMatrix(A):
 	Ca = np.dot(A,np.transpose(A))/np.trace(np.dot(A,np.transpose(A)))
 	return Ca
-
+# In[CSP]
 ### CSP算法
 filters = ()
 C_0 = covarianceMatrix(task[0][0])
@@ -62,10 +60,10 @@ C = C_0 + C_1 # 不同类别的复合空间协方差矩阵,这是一个对称矩
 E,U = la.eig(C) # 获取复合空间协方差矩阵的特征值E和特征向量U,这里C可以分解为C=np.dot(U,np.dot(np.diag(E),U.T))
 #E = E.real # E取实部；取实部后不能实现np.diag(E_0)+np.diag(E_1)=I
 
-order = np.argsort(E) # 升序排序
-order = order[::-1] # 翻转以使特征值降序排序
-E = E[order] 
-U = U[:,order]
+#order = np.argsort(E) # 升序排序
+#order = order[::-1] # 翻转以使特征值降序排序
+#E = E[order] 
+#U = U[:,order]
 
 P = np.dot(np.sqrt(la.inv(np.diag(E))),np.transpose(U)) # 获取白化变换矩阵
 
@@ -90,8 +88,7 @@ W = np.dot(np.transpose(U_0),P)
 csp = np.zeros([num_pair*2,np.shape(W)[0]]) # 提取特征的投影矩阵
 csp[0:num_pair,:] = W[0:num_pair,:] # 取投影矩阵前几行
 csp[num_pair:,:] = W[np.shape(W)[1]-num_pair:,:] # 对应取投影矩阵后几行
-
-# 利用投影矩阵提取EEG窗特征
+# In[利用CSP投影矩阵提取EEG窗特征]
 features = []
 for i in range(len(eegwin_0)):
     Z = np.dot(csp, eegwin_0[i])
@@ -106,7 +103,7 @@ for i in range(len(eegwin_1)):
     varances = [np.log(x/sum(varances)) for x in varances]
     varances.append(1)
     features.append(varances)
-    
+# In[]  
 if id_subject < 10:
     sio.savemat('E:\\EEGExoskeleton\\Data\\Subject_0'+str(id_subject)+\
                 '_Data\\Subject_0'+str(id_subject)+'_features.mat',\
